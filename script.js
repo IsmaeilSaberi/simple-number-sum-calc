d356d95cf7b1530880e9b0b64ed3ecf336618171
IsmaeilSaberi <ismaeilsaberi@gmail.com>
2023-07-19 07:53:35 -0700

    I added another form for calculation of factorielgit add .

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const number = document.querySelector("#number");
-const button = document.querySelector("#button");
+const number1 = document.querySelector("#number1");
+const button1 = document.querySelector("#button1");
 
-button.addEventListener("click", function (e) {
+button1.addEventListener("click", function (e) {
   e.preventDefault();
   let sum = 0;
-  for (let i = 1; i < number.value; i++) {
+  for (let i = 1; i < number1.value; i++) {
     sum += i;
   }
   console.log(sum);
+});
+const number2 = document.querySelector("#number2");
+const button2 = document.querySelector("#button2");
+
+button2.addEventListener("click", function (e) {
+  e.preventDefault();
+  let times = 1;
+  for (let i = 1; i < number2.value; i++) {
+    times *= i;
+  }
+  console.log(times);
 });
